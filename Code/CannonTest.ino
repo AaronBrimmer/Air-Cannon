dbe904eb4e5702d6af8623c59ea13a8956c5a19a
AaronBrimmer <45324739+AaronBrimmer@users.noreply.github.com>
2018-11-24 23:47:55 -0500

    Add files via upload

---
+++ Code/CannonTest.ino
@@ -0,0 +1,62 @@
+
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A2;  // Analog input pin that the potentiometer is attached to
+const int ButtonPin = 5; //Button Pin
+const int SwitchPin = 3; // Switch Pin
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+  // print the results to the Serial Monitor:
+  
+  Serial.print("\n");
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  Serial.print(" Analog  = ");
+  Serial.print(analogInPin);
+  Serial.print("\n");
+  Serial.print(" Button  = ");
+  Serial.print(digitalRead(ButtonPin));
+  Serial.print("\n");
+  Serial.print(" Switch  = ");
+  Serial.print(digitalRead(SwitchPin));
+  Serial.print("\n");
+  
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(1500);
+}
